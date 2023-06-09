778edf2c98ec67ea6e7d1c2f6f538c0904527c7e
eyoon1131 <eyoon1131@gmail.com>
2023-06-09 12:56:01 -0700

    Changed file name

--- tinypinball.js
+++ tinypinball.js
@@ -9,7 +9,7 @@ import { Scoreboard} from "./new-scripts/scoreboard.js";
 const {Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture} = tiny;
 const {Textured_Phong} = defs
 
-export class Assignment3 extends Scene {
+export class TinyPinball extends Scene {
   constructor() {
     // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
     super();
